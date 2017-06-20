835bbbd9605fa491ee6779f11ec1a591cc8227e8
JiangBoYang <yuyinjby@outlook.com>
2017-06-20 16:59:48 +0800

    补充设定

--- test2/test2/test2/MaxHeap.h
+++ test2/test2/test2/MaxHeap.h
@@ -1,12 +1,18 @@
 
+#include <assert.h>
+#include <string>
+using namespace std;
 template<typename Item>
-class MaxHeap
+class MaxHeap//最大堆类
 {
+public:
+
 public:
 	MaxHeap(int capacity)
 	{
-		data = new int[capacity+1];//最大树存储数组从1下标开始
+		data = new int[capacity+1];//最大树存储数组从1下标开始！！！
 		count = 0;
+		this->capacity = capacity;
 	}
 
 	~MaxHeap()
@@ -24,7 +30,28 @@ public:
 		return count == 0;
 	}
 
+	void insert(Item item)
+	{
+		assert(count+1 <= capacity);
+				
+		count ++;
+		data[count] = item;//注意最大堆从下标1开始！！
+		shiftUP(count);
+	}
+
 private:
 	Item *data;//存储最大树的数组
 	int count;//元素个数
+	int capacity;//堆容量
+
+private:
+	void shiftUP(int k)
+	{
+		while ( k>1 && data[k/2] < data[k])
+		{
+			swap(data[k/2], data[k]);
+			k /= 2;
+		}
+	}
+
 };
--- test2/test2/test2/test2.cpp
+++ test2/test2/test2/test2.cpp
@@ -3,12 +3,24 @@
 
 #include "MaxHeap.h"
 #include <iostream>
-
+#include <Windows.h>
+#include <ctime>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	MaxHeap<int> *heap = new MaxHeap<int>(10);
+	srand(time(NULL));
 
+	for (int i=0; i<30; i++)
+	{
+		heap->insert(rand()%100);
+	}
+	
+	cout << heap->size() <<endl;
+	heap->testPrint();
+	system("pause");
 	return 0;
 }
 
